d6bb2d6d79facfd0f5d60fb9ac1ec149e8a28f2d
luismuhele <luismuhele@gmail.com>
2024-07-28 20:27:18 +0100

    teste sem flask-restx

--- api/app.py
+++ api/app.py
@@ -18,12 +18,14 @@ from api.utils import send_email
 app = Flask(__name__)
 app.config.from_object(Config)
 
+"""
 api = Api(
     app,
     title="API do Elizandro",
     version="1.0",
     description="API para gerenciar as operações de um App de venda de produtos agrícolas",
 )
+"""
 db.init_app(app)
 JWTManager(app)
 CORS(app)
@@ -31,15 +33,17 @@ ma = Migrate(app, db)
 send_email.mail.init_app(app)
 
 
-@app.route("/teste")
+@app.route("/")
 def index():
-    return jsonify({"msg": "Olá mundo!"})
+    return "Olá mundo!"
 
 
+"""
 api.add_namespace(cliente_controllers.api, path="/auth/clientes")
 api.add_namespace(venda_controllers.api, path="/vendas")
 api.add_namespace(produto_controllers.api, path="/produtos")
 api.add_namespace(fornecedor_controllers.api, path="/auth/fornecedores")
+"""
 
 
 if __name__ == "__main__":
